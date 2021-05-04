e2f0b6011ddf4d91983c24660cf87e558c3292ba
Grégory D'Angelo <dangelo.gregory@gmail.com>
2021-05-04 18:36:01 +0200

    fix: use router instead of link to close menu and redirect on click + render menu using React portal

--- components/MobileMenu.js
+++ components/MobileMenu.js
@@ -1,13 +1,23 @@
-import Link from 'next/link';
+import ReactDOM from 'react-dom';
+import { useRouter } from 'next/router';
 import { signOut } from 'next-auth/client';
 import { Logo } from './index';
 import { LogoutIcon, XIcon } from '@heroicons/react/outline';
 
 const MobileMenu = ({ links = [], show = false, onClose = () => null }) => {
+  const router = useRouter();
+
+  const redirect = href => {
+    // Close menu
+    onClose();
+    // and redirect
+    router.push(href);
+  };
+
   if (!show) return null;
 
-  return (
-    <div className="absolute top-0 inset-x-0 p-2 sm:hidden">
+  return ReactDOM.createPortal(
+    <div className="fixed top-0 z-50 inset-x-0 p-2 sm:hidden">
       <div className="rounded-lg shadow-lg ring-1 ring-gray-900 dark:ring-gray-700 ring-opacity-5 bg-gray-50 text-gray-900 dark:bg-gray-900 dark:text-gray-100 divide-y divide-gray-200 dark:divide-gray-700">
         <div className="px-5 pt-4 pb-6 space-y-8">
           {/* Header */}
@@ -30,12 +40,15 @@ const MobileMenu = ({ links = [], show = false, onClose = () => null }) => {
           {/* Navigation */}
           <nav className="grid gap-y-8">
             {links.map(({ text, href, icon: Icon }) => (
-              <Link key={text} href={href}>
-                <a className="-m-3 p-3 flex items-center space-x-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800">
-                  <Icon className="flex-shrink-0 h-7 w-7 text-blue-600" />
-                  <span className="font-medium capitalize">{text}</span>
-                </a>
-              </Link>
+              <p
+                key={text}
+                href={href}
+                onClick={() => redirect(href)}
+                className="-m-3 p-3 flex items-center space-x-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800"
+              >
+                <Icon className="flex-shrink-0 h-7 w-7 text-blue-600" />
+                <span className="font-medium capitalize">{text}</span>
+              </p>
             ))}
           </nav>
         </div>
@@ -52,7 +65,8 @@ const MobileMenu = ({ links = [], show = false, onClose = () => null }) => {
           </button>
         </div>
       </div>
-    </div>
+    </div>,
+    document.documentElement
   );
 };
 
